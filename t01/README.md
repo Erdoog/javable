6f8502a7daab76fbb55e3fc9081b82c53c619506
Anwar Baimurunov <a.baimurunov@gmail.com>
2021-03-17 12:45:58 +0600

    Task 01 added

---
+++ t01/README.md
@@ -0,0 +1,4 @@
+# t01 hello world
+* Add method hello() to class T01
+* Build this file with javac
+* Run this class in terminal
---
+++ t01/T01.java
@@ -0,0 +1,6 @@
+class T01 {
+  public static void main(String[] args) {
+    T01 t01 = new T01();
+    System.out.println(t01.hello());
+  }
+}
