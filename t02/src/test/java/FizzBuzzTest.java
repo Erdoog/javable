f9ae3f052d79558311f1df42348fda30c414d446
anvar <a.baimurunov@gmail.com>
2021-03-30 12:33:26 +0600

    t2

---
+++ t02/src/test/java/FizzBuzzTest.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+  @Test
+  void FizzBuzzForFour() {
+    FizzBuzz fb = new FizzBuzz();
+    assertEquals("1\n2\nfizz\n4", fb.fizzbuzz(4));
+  }
+
+  @Test
+  void FizzBuzzForZeroShouldBeEmpty() {
+    FizzBuzz fb = new FizzBuzz();
+    assertEquals("", fb.fizzbuzz(0));
+  }
+
+  @Test
+  void FizzBuzzForNegativeShouldThrowError() {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> new FizzBuzz().fizzbuzz(-1));
+  }
+
+  @Test
+  void FizzBuzzForSixteen() {
+    FizzBuzz fb = new FizzBuzz();
+    assertEquals("1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\nfizzbuzz\n16"
+        , fb.fizzbuzz(16));
+  }
+
+}
