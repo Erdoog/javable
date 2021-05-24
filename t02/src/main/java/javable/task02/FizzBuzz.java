86faed4fa630a2b615957e20b8b33846df6c7962
Лейла Сидорова <sidorova_l0508@fmalm.nis.edu.kz>
2021-05-24 23:18:25 +0600

    t02 completed

--- t02/src/main/java/javable/task02/FizzBuzz.java
+++ t02/src/main/java/javable/task02/FizzBuzz.java
@@ -2,6 +2,24 @@ package javable.task02;
 
 class FizzBuzz {
     public String fizzbuzz(int n) {
+        String end = "";
+        if (n == -1){
+            throw new IllegalArgumentException("");
+        }
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    end = end.concat("fizzbuzz\n");
+                } else {
+                    end = end.concat("fizz\n");
+                }
+            } else if (i % 5 == 0) {
+                end = end.concat("buzz\n");
+            } else
+            end = end.concat(String.valueOf(i)+"\n");
+        }
 
+        return end;
     }
-}
+    }
+
