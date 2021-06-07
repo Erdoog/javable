dbb7896d076fc2dfc25dbb6efebad7006a51b77c
Лейла Сидорова <sidorova_l0508@fmalm.nis.edu.kz>
2021-06-08 00:47:09 +0600

    t02 completed

--- t02/src/main/java/javable/task02/FizzBuzz.java
+++ t02/src/main/java/javable/task02/FizzBuzz.java
@@ -2,37 +2,37 @@ package javable.task02;
 
 class FizzBuzz {
     public String fizzbuzz(int n) {
-        String end = "";
-        if (n == -1){
+        StringBuilder end = new StringBuilder("");
+        if (n < 0){
             throw new IllegalArgumentException("");
         }
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0) {
                 if (i % 5 == 0) {
                     if(i == n) {
-                        end = end.concat("fizzbuzz");
+                        end.append("fizzbuzz");
                     } else
-                        end = end.concat("fizzbuzz\n");
+                        end.append("fizzbuzz\n");
                 } else {
                     if(i == n) {
-                        end = end.concat("fizz");
+                        end.append("fizz");
                     } else
-                        end = end.concat("fizz\n");
+                        end.append("fizz\n");
                 }
             } else if (i % 5 == 0) {
                 if(i == n) {
-                    end = end.concat("buzz");
+                    end.append("buzz");
                 }
-                end = end.concat("buzz\n");
+                end.append("buzz\n");
             } else {
                 if(i == n) {
-                    end = end.concat(String.valueOf(i));
+                    end.append(String.valueOf(i));
                 }else
-                    end = end.concat(String.valueOf(i) + "\n");
+                    end.append(String.valueOf(i) + "\n");
             }
         }
-
-        return end;
+        String singleString = end.toString();
+        return singleString;
     }
     }
 
