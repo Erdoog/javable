29fa7c30d7597b2ee47e8f2c8369db3a6eb13fda
Лейла Сидорова <sidorova_l0508@fmalm.nis.edu.kz>
2021-06-08 19:25:55 +0600

    followed today's recommendations

--- t02/src/main/java/javable/task02/FizzBuzz.java
+++ t02/src/main/java/javable/task02/FizzBuzz.java
@@ -2,6 +2,22 @@ package javable.task02;
 
 class FizzBuzz {
     public String fizzbuzz(int n) {
-
+        StringBuilder end = new StringBuilder("");
+        if (n < 0){
+            throw new IllegalArgumentException("argument should be greater than zero")
+        }
+        for (int i = 1; i <= n; i++) {
+            if(i % 5 == 0) && (i % 3 == 0) {
+                end.append("fizzbuzz");
+            } else if (i % 5 == 0) {
+                end.append("buzz");
+            } else if (i % 3 == 0) {
+                end.append("fizz");
+            } else {
+                end.append(String.valueOf(i));
+            }
+            end.append(i < n? "\n": "")
+        }
+        return end.toString();
     }
 }
