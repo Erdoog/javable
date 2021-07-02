d49022d8b8a0ed6714520ffeca579aea17152cbe
Лейла Сидорова <sidorova_l0508@fmalm.nis.edu.kz>
2021-07-03 02:48:33 +0600

    I think something is wrong here, but it still works (T03)

--- t03/src/main/java/javable/t03/BubbleListSorter.java
+++ t03/src/main/java/javable/t03/BubbleListSorter.java
@@ -3,8 +3,24 @@ package javable.t03;
 import java.util.List;
 
 public class BubbleListSorter<T extends Comparable<T>> implements ListSorter<T> {
+
     @Override
     public void sort(List<T> list) {
+        T temp;
+        boolean sorted = false;
 
-    }
+        while (!sorted) {
+            sorted = true;
+            if(list != null) {
+                for (int i = 0; i < list.size() - 1; i++) {
+                    if (list.get(i).compareTo(list.get(i + 1)) > 0) {
+                        temp = list.get(i);
+                        list.set(i, list.get(i + 1));
+                        list.set(i + 1, temp);
+                        sorted = false;
+                    }
+                }
+            }
+        }
+        }
 }
--- t03/src/main/java/javable/t03/SelectionListSorter.java
+++ t03/src/main/java/javable/t03/SelectionListSorter.java
@@ -5,6 +5,19 @@ import java.util.List;
 public class SelectionListSorter<T extends Comparable<T>> implements ListSorter<T> {
     @Override
     public void sort(List<T> list) {
+        int size = list.size();
+        T temp;
+        for (int step = 0; step < size - 1; step++) {
+            int min_idx = step;
 
+            for (int i = step + 1; i < size; i++) {
+                if (list.get(i).compareTo(list.get(min_idx)) < 0) {
+                    min_idx = i;
+                }
+            }
+            temp = list.get(step);
+            list.set(step, list.get(min_idx));
+            list.set(min_idx, temp);
+        }
     }
 }
