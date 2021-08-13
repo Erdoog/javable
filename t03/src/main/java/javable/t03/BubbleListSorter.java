67a3f06556d8ffbadd90fc7e3e09a376026fedeb
Лейла Сидорова <sidorova_l0508@fmalm.nis.edu.kz>
2021-08-13 09:13:46 +0600

    modified BubbleListSorter

--- t03/src/main/java/javable/t03/BubbleListSorter.java
+++ t03/src/main/java/javable/t03/BubbleListSorter.java
@@ -7,22 +7,17 @@ public class BubbleListSorter<T extends Comparable<T>> implements ListSorter<T> 
     @Override
     public void sort(List<T> list) {
         T temp;
-        boolean sorted = false;
         if(list == null || list.isEmpty()) {
             return;
         }
-        while (!sorted) {
-            sorted = true;
-
-                for (int i = 0; i < list.size() - 1; i++) {
-                    if (list.get(i).compareTo(list.get(i + 1)) > 0) {
-                        temp = list.get(i);
-                        list.set(i, list.get(i + 1));
-                        list.set(i + 1, temp);
-                        sorted = false;
-                    }
+        for (int x=0; x<list.size(); x++){
+            for (int i=0; i < list.size()-1; i++) {
+                if (list.get(i).compareTo(list.get(i + 1)) > 0) {
+                    temp = list.get(i);
+                    list.set(i,list.get(i+1) );
+                    list.set(i+1, temp);
                 }
-
+            }
         }
-        }
+    }
 }
